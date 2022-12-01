b46617e4a60eefac8a24f96ae36eb8acba87408c
lebaonhi12c1 <lebaonhi12c1@gmail.com>
2022-12-01 09:23:00 +0700

    update source

--- jquery/main.js
+++ jquery/main.js
@@ -1,3 +1,16 @@
 $(function(){
-    console.log('hello world')
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://throne-of-glass-api-2.p.rapidapi.com/api/characters",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "1658ff66aamsh699b709b3f58e9cp117eb4jsnef6fcb8655ea",
+            "X-RapidAPI-Host": "throne-of-glass-api-2.p.rapidapi.com"
+        }
+    };
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
 })
