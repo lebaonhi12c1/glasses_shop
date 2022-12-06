cce05ff67026f28bd411d8547ecd0eb6e5ca3269
lebaonhi12c1 <lebaonhi12c1@gmail.com>
2022-12-06 23:24:37 +0700

    update source

--- jquery/main.js
+++ jquery/main.js
@@ -11,4 +11,26 @@ $(function(){
         e.preventDefault();
         document.location.href= 'http://127.0.0.1:5500/product.html'
     });
+    var windowH = $(window).height() / 2;
+
+    $(window).on("scroll", function () {
+    if ($(this).scrollTop() > windowH) {
+      $(".toTop").css("display", "flex");
+    } else {
+      $(".toTop").css("display", "none");
+    }
+    });
+
+    $(".toTop").on("click", function () {
+        $("html, body").animate({ scrollTop: 0 }, 300);
+    });
+    var active_index = -1;
+    $('.nav__menu__link').each(function (index, element) {
+        $(element).click(function (e) { 
+            active_index = index;
+        });
+    });
+    if(active_index ===0){
+        
+    }
 })
