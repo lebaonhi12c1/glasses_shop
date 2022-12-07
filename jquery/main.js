220906f126cc21412ba9b4fb49d6cbc7615d7967
lebaonhi12c1 <lebaonhi12c1@gmail.com>
2022-12-07 09:12:10 +0700

    up more js

--- jquery/main.js
+++ jquery/main.js
@@ -2,6 +2,7 @@ $(function(){
     $('.logo').click(function (e) { 
         e.preventDefault();
         document.location.href = 'http://127.0.0.1:5500/index.html'
+        sessionStorage.clear()
     });
     $('.product_item').click(function (e) { 
         e.preventDefault();
@@ -24,13 +25,19 @@ $(function(){
     $(".toTop").on("click", function () {
         $("html, body").animate({ scrollTop: 0 }, 300);
     });
-    var active_index = -1;
     $('.nav__menu__link').each(function (index, element) {
+        // element == this
+        $(element).addClass('navlink');
+        $(element).removeClass('text-black');
+        $(element).attr('id', $(element).text());
+        console.log(element)
         $(element).click(function (e) { 
-            active_index = index;
+          sessionStorage.setItem('active_header',  $(element).text())
         });
     });
-    if(active_index ===0){
-        
-    }
+
+    $('.nav__menu__link').each(function (index, element) {
+        console.log($(element).text(),sessionStorage.getItem('active_header'))
+        $(element).text() === sessionStorage.getItem('active_header') &&$(element).addClass('text-danger')
+    });
 })
